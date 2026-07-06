f068b97b6c0c0a1b503eb22edc532a3d01298bdd
windowsvistafan2097 <wvistafan2007@gmail.com>
2026-07-06 17:35:34 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . . # . .
+        . # . # .
+        `)
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # # # # .
+    . . . . .
+    `)
